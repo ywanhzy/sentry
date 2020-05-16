963f89e34f21b9ae4d717b4040fcd78054ec477b
Alberto Leal <mail4alberto@gmail.com>
2020-05-16 09:56:02 -0400

    adjust types

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanTree.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanTree.tsx
@@ -8,7 +8,7 @@ import {TableData} from 'app/views/eventsV2/table/types';
 
 import {
   ProcessedSpanType,
-  RawSpanType,
+  SpanType,
   SpanChildrenLookupType,
   ParsedTraceType,
   GapSpanType,
@@ -106,7 +106,7 @@ class SpanTree extends React.Component<PropType> {
     return <SpanRowMessage>{messages}</SpanRowMessage>;
   }
 
-  isSpanFilteredOut(span: Readonly<RawSpanType>): boolean {
+  isSpanFilteredOut(span: Readonly<SpanType>): boolean {
     const {filterSpans} = this.props;
 
     if (!filterSpans) {
@@ -144,7 +144,7 @@ class SpanTree extends React.Component<PropType> {
     const {orgId, eventView, event, spansWithErrors} = this.props;
 
     const spanBarColour: string = pickSpanBarColour(getSpanOperation(span));
-    const spanChildren: Array<RawSpanType> = childSpans?.[getSpanID(span)] ?? [];
+    const spanChildren: Array<SpanType> = childSpans?.[getSpanID(span)] ?? [];
 
     const bounds = generateBounds({
       startTimestamp: span.start_timestamp,
@@ -300,7 +300,7 @@ class SpanTree extends React.Component<PropType> {
   renderRootSpan = (): RenderedSpanTree => {
     const {dragProps, trace} = this.props;
 
-    const rootSpan: RawSpanType = generateRootSpan(trace);
+    const rootSpan: SpanType = generateRootSpan(trace);
 
     const generateBounds = boundsGenerator({
       traceStartTimestamp: trace.traceStartTimestamp,
