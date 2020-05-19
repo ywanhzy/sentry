5f348e25081afa2ae13ee5f67364ee2296f24d9c
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-19 14:23:46 +0200

    ref(ui): converted suggestedOwners to ts

--- src/sentry/static/sentry/app/types/index.tsx
+++ src/sentry/static/sentry/app/types/index.tsx
@@ -64,10 +64,8 @@ export type Avatar = {
   avatarType: 'letter_avatar' | 'upload' | 'gravatar';
 };
 
-export type Actor = {
-  id: string;
+export type Actor = User & {
   type: 'user' | 'team';
-  name: string;
 };
 
 /**
@@ -342,9 +340,10 @@ type UserEnrolledAuthenticator = {
   id: EnrolledAuthenticator['authId'];
 };
 
-export type User = AvatarUser & {
+export type User = Omit<AvatarUser, 'options'> & {
   lastLogin: string;
   isSuperuser: boolean;
+  isAuthenticated: boolean;
   emails: {
     is_verified: boolean;
     id: string;
